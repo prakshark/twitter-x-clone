0a32228c0d75e38df9b406c86c85d19674379eaf
prakshark <prakhar112358@gmail.com>
2025-06-20 22:17:42 +0530

    css fixing in dep

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,22 +1,29 @@
 import daisyui from "daisyui";
-import daisyUIThemes from "daisyui/src/theming/themes";
+import { black } from "daisyui/src/theming/themes";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [daisyui],
-
-	daisyui: {
-		themes: [
-			{
-				black: {
-					...daisyUIThemes["black"],
-					primary: "rgb(29, 155, 240)",
-					secondary: "rgb(24, 24, 24)",
-				},
-			},
-		],
-	},
-};
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: [
+      {
+        black: {
+          ...black, // Spread the default black theme properties
+          "primary": "rgb(29, 155, 240)",
+          "secondary": "rgb(24, 24, 24)",
+          // You can add more customizations here:
+          // "accent": "#somecolor",
+          // "neutral": "#somecolor",
+          // etc.
+        }
+      }
+    ],
+  }
+}
